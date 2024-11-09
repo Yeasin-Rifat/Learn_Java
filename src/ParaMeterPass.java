3e5103773e1aed89b9bed27ba47c8621fee3e58a
Yeasin-Rifat <Yeasinrifat1998@gmail.com>
2024-11-10 01:36:29 +0600

    1st commit

---
+++ src/ParaMeterPass.java
@@ -0,0 +1,11 @@
+public class ParaMeterPass {
+    static void myMethod(String fname){
+System.out.println(fname + " Islam");
+    }
+    public static void main(String[] args) {
+        myMethod("Rifat");
+        myMethod("Jashim");
+        myMethod("Rahim");
+
+    }
+}
