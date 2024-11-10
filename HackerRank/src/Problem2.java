cb95c424f5637fa513b0dfebff2dcbead0ce3544
Yeasin-Rifat <yeasinrifat1998@gmail.com>
2024-11-10 15:58:45 +0600

    Add files via upload

---
+++ HackerRank/src/Problem2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Problem2 {
+    public static void main(String[] args) {
+        Scanner obj1 = new Scanner(System.in);
+        int a= obj1.nextInt();
+        double b= obj1.nextDouble();
+        obj1.nextLine();
+        String str = obj1.nextLine();
+
+        System.out.println("String: " + str);
+        System.out.println("Double: " + b);
+        System.out.println("Int: " + a);
+
+
+
+    }
+}
